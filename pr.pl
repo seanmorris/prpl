9cea406c815f7ffebbfc1c460a264d7bc8ec2bfa
Sean Morris <sean@seanmorr.is>
2019-10-14 06:44:11 -0400

    Adding git pre-commit hooks.

--- pr.pl
+++ pr.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/env perl
+
+# Prpl © Copyright 2019 Sean Morris
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Visit https://github.com/seanmorris/prpl for help, or to contribute.
+
 use strict;
 use warnings;
 
@@ -8,12 +25,55 @@ my $operation;
 my $delimiter;
 my $expression;
 my $modifiers = '';
-my %flags = ('v', 0); 
+my %flags = ('v', 0);
 
 while($ARGV[0] && $ARGV[0] =~ /^-(.)/)
 {
 	shift @ARGV;
 	$flags{ $1 } = 1;
+}
+
+if($flags{ 'i' })
+{
+	print "\n";
+	open(my $script, $0);
+
+	my $line = 0;
+
+	for(<$script>)
+	{
+		if($line > 0 && /^#((?:\s.+)?)/)
+		{
+			print $1 . "\n";
+		}
+		$line++;
+	}
+	print "\n";
+
+	exit 0;
+}
+
+if($flags{ 'h' })
+{
+	print "Usage:\n";
+	print "prpl [-v] PATTERN [file, file...]\n";
+	print "\n";
+	print "-v Invert match (return only non-matching lines)\n";
+	print "   Available for matching only, will throw warnings for\n";
+	print "   replacements or transliteration.\n";
+	print "\n   PATTERN is a PCRE matching pattern prefixed with...";
+	print "\n";
+	print "\n      m for matching        - /.+?/ (optional, matching is default)";
+	print "\n      s for replacement     - s/gr[ae]y/black/";
+	print "\n      m for transliteration - y/A-Za-z/N-ZA-Mn-za-m/";
+	print "\n";
+	print "\n";
+	print "-h Print this help message.\n";
+	print "\n";
+	print "-i Print version & copyright information.\n";
+	print "\n";
+
+	exit 0;
 }
 
 if(!$ARGV[0])
@@ -72,7 +132,7 @@ for(@files)
 			{
 				if($flags{ 'v' })
 				{
-					print; print "\n";					
+					print; print "\n";
 				}
 			}
 		}
--- test/test.sh
+++ test/test.sh
@@ -1,48 +1,33 @@
 PASSED=0;
 FAILED=0;
 
-for INPUT in $(cat grepTest.txt); do
+prpl -i;
 
+echo "Starting tests..."
+
+for INPUT in $(cat grepTest.txt); do
 	PRPL=$(prpl /$INPUT/ dictionary.txt)
 	GREP=$(cat dictionary.txt | grep $INPUT)
-
 	if [[ $PRPL != $GREP ]]; then
-
 		echo "Mismatch between prpl & grub for search $INPUT'"
-
 		FAILED=$((FAILED+1))
-
 	else
-
 		PASSED=$((PASSED+1))
-
 	fi;
-
 done;
 
 
-
 for REPLACE_INPUT in $(cat sedTest.txt); do
-
 	for SEARCH_INPUT in $(cat grepTest.txt); do
-
 		GREP=$(cat dictionary.txt | grep $SEARCH_INPUT)
-
 		for INPUT in $GREP; do
-
 			PRPL=$(echo $INPUT | prpl $REPLACE_INPUT)
 			SED=$(echo $INPUT | sed -e $REPLACE_INPUT)
-
 			if [[ $PRPL != $SED ]]; then
-
 				echo "Mismatch between prpl & sed for $INPUT =~ $REPLACE_INPUT'"
-
 				FAILED=$((FAILED+1))
-
 			else
-
 				PASSED=$((PASSED+1))
-
 			fi;
 		done;
 	done;
@@ -51,9 +36,7 @@ done;
 echo "$PASSED test passed. $FAILED tests failed."
 
 if [ $FAILED != 0 ]; then
-
 	exit 1;
-
 fi;
 
 exit 0;
